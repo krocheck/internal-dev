9c4d8c6b43eaf5808a7cd498c5aa7fbfe99cdc45
Keith Rocheck <749812+krocheck@users.noreply.github.com>
2019-01-14 21:34:58 -0600

    rosstalk switcher profiles

--- rosstalk/rosstalk.js
+++ rosstalk/rosstalk.js
@@ -21,6 +21,100 @@ instance.prototype.updateConfig = function(config) {
 	self.init_tcp();
 };
 
+instance.prototype.CONFIG_MODEL = {
+	1:  { id: 1,  label: 'Carbonite',                arch: 'carbonite', inputs: 24,  MEs: 2, keyers: 4, MMEs: 4,  MMEKeyers: 2, MSCs: 2,  XP: 0, CK: 0, MSs: '4|0',  MVs: '2|16', CC: '8|32',  mem: 100,  auxes: '1|8' },
+	2:  { id: 2,  label: 'Carbonite Multi-Media',    arch: 'carbonite', inputs: 24,  MEs: 2, keyers: 4, MMEs: 4,  MMEKeyers: 2, MSCs: 2,  XP: 0, CK: 0, MSs: '4|0',  MVs: '2|16', CC: '8|32',  mem: 100,  auxes: '1|8' },
+	3:  { id: 3,  label: 'Carbonite Plus',           arch: 'carbonite', inputs: 24,  MEs: 2, keyers: 4, MMEs: 4,  MMEKeyers: 2, MSCs: 2,  XP: 0, CK: 0, MSs: '4|0',  MVs: '2|16', CC: '8|32',  mem: 100,  auxes: '1|8' },
+	4:  { id: 4,  label: 'Carbonite Black',          arch: 'carbonite', inputs: 36,  MEs: 3, keyers: 4, MMEs: 4,  MMEKeyers: 2, MSCs: 2,  XP: 0, CK: 0, MSs: '4|0',  MVs: '2|16', CC: '8|32',  mem: 100,  auxes: '1|20' },
+	5:  { id: 5,  label: 'Carbonite Black UHD',      arch: 'carbonite', inputs: 9,   MEs: 1, keyers: 4, MMEs: 2,  MMEKeyers: 2, MSCs: 2,  XP: 0, CK: 2, MSs: '2|0',  MVs: '1|16', CC: '8|32',  mem: 100,  auxes: '1|10' },
+	6:  { id: 6,  label: 'Carbonite Black+',         arch: 'carbonite', inputs: 36,  MEs: 3, keyers: 4, MMEs: 4,  MMEKeyers: 2, MSCs: 2,  XP: 0, CK: 0, MSs: '4|0',  MVs: '5|16', CC: '8|32',  mem: 100,  auxes: '1|20' },
+	7:  { id: 7,  label: 'Carbonite Black+ UHD',     arch: 'carbonite', inputs: 9,   MEs: 1, keyers: 4, MMEs: 2,  MMEKeyers: 2, MSCs: 2,  XP: 0, CK: 2, MSs: '2|0',  MVs: '4|16', CC: '8|32',  mem: 100,  auxes: '1|10' },
+	8:  { id: 8,  label: 'Carbonite Black Solo9',    arch: 'carbonite', inputs: 9,   MEs: 1, keyers: 4, MMEs: 2,  MMEKeyers: 2, MSCs: 1,  XP: 0, CK: 0, MSs: '4|0',  MVs: '1|10', CC: '8|32',  mem: 100,  auxes: '1|16' },
+	9:  { id: 9,  label: 'Carbonite Black Solo13',   arch: 'carbonite', inputs: 13,  MEs: 1, keyers: 4, MMEs: 2,  MMEKeyers: 2, MSCs: 1,  XP: 0, CK: 0, MSs: '4|0',  MVs: '1|10', CC: '8|32',  mem: 100,  auxes: '1|16' },
+	10: { id: 10, label: 'Carbonite Ultra',          arch: 'carbonite', inputs: 24,  MEs: 3, keyers: 4, MMEs: 4,  MMEKeyers: 2, MSCs: 2,  XP: 0, CK: 2, MSs: '4|0',  MVs: '4|16', CC: '8|32',  mem: 100,  auxes: '1|28' },
+	11: { id: 11, label: 'Carbonite Ultra UHD',      arch: 'carbonite', inputs: 18,  MEs: 2, keyers: 2, MMEs: 2,  MMEKeyers: 2, MSCs: 1,  XP: 0, CK: 2, MSs: '2|0',  MVs: '4|16', CC: '8|32',  mem: 100,  auxes: '1|28' },
+	12: { id: 12, label: 'Carbonite Mosaic',         arch: 'carbonite', inputs: 36,  MEs: 0, keyers: 0, MMEs: 10, MMEKeyers: 4, MSCs: 10, XP: 0, CK: 0, MSs: '36|0', MVs: '4|16', CC: '8|32',  mem: 100,  auxes: '1|20' },
+	13: { id: 13, label: 'Carbonite UltraChromeHR',  arch: 'carbonite', inputs: 36,  MEs: 0, keyers: 0, MMEs: 4,  MMEKeyers: 2, MSCs: 0,  XP: 0, CK: 4, MSs: '8|0',  MVs: '2|16', CC: '0|0',   mem: 100,  auxes: '1|20' },
+	14: { id: 14, label: 'Carbonite UltraChromeHR+', arch: 'carbonite', inputs: 36,  MEs: 0, keyers: 0, MMEs: 4,  MMEKeyers: 2, MSCs: 0,  XP: 0, CK: 4, MSs: '8|0',  MVs: '5|16', CC: '0|0',   mem: 100,  auxes: '1|20' },
+	15: { id: 15, label: 'Graphite',                 arch: 'carbonite', inputs: 13,  MEs: 2, keyers: 4, MMEs: 4,  MMEKeyers: 2, MSCs: 2,  XP: 3, CK: 0, MSs: '4|0',  MVs: '2|16', CC: '8|32',  mem: 100,  auxes: '1|20' },
+	16: { id: 16, label: 'Acuity 4RU',               arch: 'acuity',    inputs: 60,  MEs: 7, keyers: 8, MMEs: 10, MMEKeyers: 1, MSCs: 0,  XP: 0, CK: 0, MSs: '4|4',  MVs: '6|20', CC: '48|48', mem: 1000, auxes: '6|8' },
+	17: { id: 17, label: 'Acuity 8RU',               arch: 'acuity',    inputs: 120, MEs: 8, keyers: 8, MMEs: 15, MMEKeyers: 1, MSCs: 0,  XP: 0, CK: 0, MSs: '4|4',  MVs: '8|20', CC: '48|48', mem: 1000, auxes: '6|8' },
+	18: { id: 18, label: 'Vision MD',                arch: 'vision',    inputs: 32,  MEs: 3, keyers: 4, MMEs: 0,  MMEKeyers: 0, MSCs: 0,  XP: 0, CK: 0, MSs: '3|4',  MVs: '0|0',  CC: '12|48', mem: 100,  auxes: '6|8' },
+	19: { id: 19, label: 'Vision QMD',               arch: 'vision',    inputs: 32,  MEs: 5, keyers: 4, MMEs: 8,  MMEKeyers: 1, MSCs: 0,  XP: 0, CK: 0, MSs: '3|4',  MVs: '0|0',  CC: '12|48', mem: 100,  auxes: '6|8' },
+	20: { id: 20, label: 'Vision MD-X',              arch: 'vision',    inputs: 96,  MEs: 4, keyers: 4, MMEs: 0,  MMEKeyers: 0, MSCs: 0,  XP: 0, CK: 0, MSs: '3|4',  MVs: '0|0',  CC: '12|48', mem: 100,  auxes: '6|8' },
+	21: { id: 21, label: 'Vision QMD-X',             arch: 'vision',    inputs: 96,  MEs: 8, keyers: 4, MMEs: 12, MMEKeyers: 1, MSCs: 0,  XP: 0, CK: 0, MSs: '3|4',  MVs: '0|0',  CC: '12|48', mem: 100,  auxes: '6|8' },
+};
+
+instance.prototype.CONFIG_BUSES = [
+	{ bus: 'ME',  carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'MME', carbonite: 1, acuity: 0, vision: 0 },
+	{ bus: 'MSC', carbonite: 1, acuity: 0, vision: 0 }
+];
+
+instance.prototype.CONFIG_DEST = [
+	{ bus: 'AUX', carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'KEY', carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'ME',  carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'MME', carbonite: 1, acuity: 0, vision: 0 },
+	{ bus: 'PGM', carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'PST', carbonite: 1, acuity: 1, vision: 1 }
+];
+
+instance.prototype.CONFIG_SRC = [
+	{ bus: 'AUX', carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'BG',  carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'BK',  carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'CLN', carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'CK',  carbonite: 1, acuity: 0, vision: 0 },
+	{ bus: 'CKA', carbonite: 1, acuity: 0, vision: 0 },
+	{ bus: 'GS',  carbonite: 0, acuity: 1, vision: 1 },
+	{ bus: 'IN',  carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'KEY', carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'ME',  carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'MME', carbonite: 1, acuity: 0, vision: 0 },
+	{ bus: 'MS',  carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'MVA', carbonite: 0, acuity: 1, vision: 0 },
+	{ bus: 'MVB', carbonite: 0, acuity: 1, vision: 0 },
+	{ bus: 'PGM', carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'PV',  carbonite: 1, acuity: 1, vision: 1 },
+	{ bus: 'XPA', carbonite: 1, acuity: 0, vision: 0 },
+	{ bus: 'XPV', carbonite: 1, acuity: 0, vision: 0 }
+];
+
+instance.prototype.CONFIG_COMMANDS = [
+	{ command: 'CC',             carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'CKINIT',         carbonite: 1, acuity: 0, vision: 0 },
+	{ command: 'FTB',            carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'GPI',            carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'HELP',           carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'KEYAUTO',        carbonite: 0, acuity: 1, vision: 1 },
+	{ command: 'KEYAUTOON',      carbonite: 1, acuity: 0, vision: 0 },
+	{ command: 'KEYAUTOOFF',     carbonite: 1, acuity: 0, vision: 0 },
+	{ command: 'KEYCUT',         carbonite: 0, acuity: 1, vision: 1 },
+	{ command: 'KEYCUTON',       carbonite: 1, acuity: 0, vision: 0 },
+	{ command: 'KEYCUTOFF',      carbonite: 1, acuity: 0, vision: 0 },
+	{ command: 'KEYMODE',        carbonite: 1, acuity: 0, vision: 0 },
+	{ command: 'KEYSHAPED',      carbonite: 0, acuity: 1, vision: 1 },
+	{ command: 'KEYSTATE',       carbonite: 0, acuity: 1, vision: 1 },
+	{ command: 'LOADSET',        carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'MEAUTO',         carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'MECUT',          carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'MEM',            carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'MEMSAVE',        carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'MENM',           carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'MS',             carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'MSPLAY',         carbonite: 0, acuity: 1, vision: 1 },
+	{ command: 'MVBOX',          carbonite: 1, acuity: 1, vision: 0 },
+	{ command: 'RESETALL',       carbonite: 0, acuity: 1, vision: 1 },
+	{ command: 'SAVESET',        carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'SETVIDMODE REF', carbonite: 0, acuity: 1, vision: 1 },
+	{ command: 'SETVIDMODE VID', carbonite: 0, acuity: 1, vision: 1 },
+	{ command: 'TRANSINCL',      carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'TRANSRATE',      carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'TRANSTYPE',      carbonite: 1, acuity: 1, vision: 1 },
+	{ command: 'XPT',            carbonite: 0, acuity: 1, vision: 1 }
+];
+
 instance.prototype.init = function() {
 	var self = this;
 
